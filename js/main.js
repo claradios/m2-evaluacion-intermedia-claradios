166404203b4286f5ab942a68c699de85002ba1c2
claradios <clara.dios.diez@gmail.com>
2019-08-04 11:30:32 +0200

    Manage when the user hits the button, and the input is empty

--- js/main.js
+++ js/main.js
@@ -28,22 +28,28 @@ function guessGame(){
         footerText.innerHTML= '¡HAS GANADO, CAMPEONA!';
     }
     else if (inputNumber >100) {
-        footerText.innerHTML = '¡¿Dónde vas?! el número ha de ser menor que 100';
+        footerText.innerHTML = '<strong>¡¿Dónde vas?!</strong> El número ha de ser menor que 100';
     }
     else if (inputNumber<0) {
-        footerText.innerHTML = '¡no se acepta tanta negatividad!'
+        footerText.innerHTML = '<strong>¡no se acepta tanta negatividad!</strong>Es un número positivo'
     }
     else if (inputNumber > myRandomNumber){
         footerText.innerHTML = 'el número es muy alto';
+        tries++;
+        counter.innerHTML = tries;
     }
     else if (inputNumber < myRandomNumber){
         footerText.innerHTML = 'el número es muy bajo';
-    }
-    else {
-        footerText.innerHTML= 'has de introducir un número!';
-    }        
         tries++;
         counter.innerHTML = tries;
+    }
+    else if(input.value === ""){
+        footerText.innerHTML = '<strong>De donde no hay no se puede sacar</strong>, introduce un número por favor.';
+    }
+    else {
+        footerText.innerHTML= '<strong>No funciona así.</strong>¡Has de introducir un número, no una letra!';
+    }        
+        
 }
 
 //escucho al botón de comprobar 
